d7402fbd44996ea1c9342f462a124e15e45b4601
JpRoGarcia <juan.1020405@gmail.com>
2023-09-06 17:28:46 -0500

    Un dia Mas

--- services/db.js
+++ services/db.js
@@ -3,11 +3,11 @@ const pg = require("pg");
 //Conexion con Base de Datos 
 const execute = async (sql) => {
     const client = new pg.Client({
-        user: 'postgres',
-        host: 'dataBase',
-        database: 'apimusic',
-        password: 'apimusic',
+        host: 'appmusic.cdk5irdeokab.us-east-1.rds.amazonaws.com',
         port: 5432,
+        user: 'postgres', 
+        database: 'appmusic',
+        password: 'apimusic',
       });
 
     await client.connect();
