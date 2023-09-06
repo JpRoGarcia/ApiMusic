db474965c8c164b0dc13eed7dc090fcc33611984
JpRoGarcia <juan.1020405@gmail.com>
2023-09-06 18:48:07 -0500

    - RSD AWS

--- services/db.js
+++ services/db.js
@@ -3,14 +3,20 @@ const pg = require("pg");
 //Conexion con Base de Datos 
 const execute = async (sql) => {
     const client = new pg.Client({
+        user: 'postgres',
         host: 'appmusic.cdk5irdeokab.us-east-1.rds.amazonaws.com',
-        port: 5432,
-        user: 'postgres', 
         database: 'appmusic',
         password: 'apimusic',
+        port: 5432,
       });
 
-    await client.connect();
+    await client.connect((err) => {
+      if(err) {
+        console.log(err.message);
+        return;
+      }
+      console.log("Base Datos Conectada")
+    });
     const res = await client.query(sql);
     await client.end();
     return res
