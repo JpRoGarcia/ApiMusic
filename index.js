69d533033d287e08989f8c13ccf31dfe1d10f496
JpRoGarcia <juan.1020405@gmail.com>
2023-10-10 23:21:46 -0500

    cambio puerto

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', function (req, res) {
 })
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
   //console.log(`API node: http://localhost:${PORT}/`);
