50d5bef2db2bd36c7e81c21fd63e631703926059
JpRoGarcia <juan.1020405@gmail.com>
2023-11-01 23:52:21 -0500

    Probando Terra

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "name" {
+resource "aws_instance" "server" {
   ami = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.micro"
   key_name = aws_key_pair.deployer.key_name
